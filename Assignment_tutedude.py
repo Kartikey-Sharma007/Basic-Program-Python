f2f4e69697d258227cd217cf53e66dc0350d82db
Kartikey Sharma <kartikeys84@gmail.com>
2025-07-26 06:28:16 +0200

    Add files via upload

---
+++ Assignment_tutedude.py
@@ -0,0 +1,27 @@
+#task 1 assisgnment
+'''a=input('enetr the value of num 1')
+a=float(a)
+b=input('enetr the value of num 2')
+b=float(b)
+c=a+b
+print('addition=' ,c)
+d=a-b
+d=float(d)
+print('substraction=' ,d)
+e=a*b
+e=float(e)
+print('Multiplication=' ,e)
+f=a/b
+f=float(f)
+print('Division=' ,f)
+'''
+
+
+#task 2 Assisgnment
+'''a1=input('Enter the first name')
+a2=input('Enter the last name')
+b=a1
+c=a2
+d="! Welcome to Python program."
+print('Hello,',b, c, d)
+'''
